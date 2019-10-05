659ca433c5935511b94351a638ba0985b91d3baa
wuyeelok <wuyeelok@gmail.com>
2019-10-05 17:45:37 +0800

    Use ClassPathXmlApplicationContext istead of BF

--- src/main/java/com/kenneth/SpringDemo/App.java
+++ src/main/java/com/kenneth/SpringDemo/App.java
@@ -1,13 +1,15 @@
 package com.kenneth.SpringDemo;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
 
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		// BeanFactory factory1 = new XmlBeanFactory(new
+		// FileSystemResource("spring.xml"));
+
+		ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
 
 		Alien obj = factory.getBean("navinAlien", Alien.class);
 
